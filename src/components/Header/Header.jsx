9349f3421678295312efbd818ccaa81f0b37c711
mainodddin rakib <mainoddinrakib7@gmail.com>
2025-01-23 22:39:00 +0600

    Updated

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h1 className="header-title">Order Your Favourite food here</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum, nemo.</p>
+                 <button>View Menu</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { assets } from './../../assets/frontend_assets/assets';
+
+const Navbar = () => {
+
+    const [menu, setMenu] = useState("home");
+
+    return (
+        <div className="navbar flex justify-between items-center">
+            <img src={assets.logo} alt="" className='logo w-[150px]' />
+            <ul className='navbar-menu flex list-none gap-[20px]  text-[#49557e] font-semibold cursor-pointer '>
+                <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
+                <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
+                <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile-App</li>
+                <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact us</li>
+            </ul>
+            <div className="navbar-right flex items-center gap-[40px] ">
+                <img src={assets.search_icon} alt="" />
+                <div className="navbar-search-icon relative">
+                    <img src={assets.basket_icon} alt="" />
+                    <div className="dot absolute min-w-[10px] min-h-[10px] bg-red-500 rounded-[5px] top-[-8px] right-[-8px] cursor-pointer"></div>
+                </div>
+                <div className="bg-transparent
+                   border text-black font-bold
+                   px-[30px] py-[10px] rounded
+                   hover:bg-[#fff4f2]
+                    hover:text-black transition duration-200 rounded-[50px] cursor-pointer">
+                    Sign-In
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
